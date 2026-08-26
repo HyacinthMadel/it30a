c3fed358331a85e3ba6dc21d50198d382cf6e422
HyacinthMadel <hyacinth.udtohan08@gmail.com>
2026-08-26 15:35:20 +0800

    added books

---
+++ lab3.sql
@@ -0,0 +1,46 @@
+CREATE TABLE books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    book_title VARCHAR (50) NOT NULL,
+    book_author VARCHAR(50) NOT NULL,
+    book_category VARCHAR(50) NOT NULL,
+    book_created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+
+INSERT INTO books (book_title,book_author,book_category) VALUES ("Project Loki", "None", "Mystery"), ("Rain In España", "Gween", "Romance"), ("Seducing Drake Palma", "Alice", "Rom-Com");
+
+
+
+CREATE TABLE borrow(
+       borrow_id INT PRIMARY KEY AUTO_INCREMENT,
+
+       student_id INT NOT NULL,
+       book_id INT NOT NULL,
+
+       borrow_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+       borrow_return_date TIMESTAMP NULL DEFAULT NULL,
+       
+       CONSTRAINT fk_borrow_student FOREIGN KEY (student_id) 
+       REFERENCES student(student_id), 
+
+       CONSTRAINT fk_borrow_book FOREIGN KEY (book_id) 
+       REFERENCES books(book_id)
+);
+
+INSERT INTO borrow (student_id,book_id) VALUES (1,2), (2,1), (3,3);
+
+
+SELECT
+    br.borrow_id,
+    s.student_id,
+    CONCAT(s.student_first_name, ' ', s.student_last_name) AS student_name,
+    s.student_course,
+    b.book_title,
+    b.book_author,
+    b.book_category,
+    br.borrow_date
+FROM borrow br
+JOIN student s ON br.student_id = s.student_id
+JOIN books b ON br.book_id = b.book_id
+WHERE br.borrow_return_date IS NULL
+ORDER BY br.borrow_date DESC;
